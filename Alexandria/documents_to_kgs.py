8e02f0e7a9ee7ff9ebab3893b73286773eda195a
Christoph Schuhmann <christoph_s@freenet.de>
2025-01-07 16:42:12 +0100

    Update documents_to_kgs.py

--- Alexandria/documents_to_kgs.py
+++ Alexandria/documents_to_kgs.py
@@ -338,7 +338,7 @@ def KG_construction(input_text, model, preprocessing_type="none"):
         current_kg_context = ' '.join(current_kg_context)
 
         # Generate the prompt for knowledge graph construction
-        text = scripts.prompts.KG_format_example_prompt2(current_kg_context, sentence)
+        text = scripts.prompts.KG_format_example_prompt_many_examplesplus(current_kg_context, sentence)
         #print("Prompt:", text)
 
         # Generate the knowledge graph segment
